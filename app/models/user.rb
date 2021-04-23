3eda595179fa1a9fa219fea3bf42510580fe3aaa
chrislemus <icrislemus@gmail.com>
2021-04-23 19:33:57 -0400

    added email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: {case_sensitive: false},format:{ with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i,
+  validates :email, confirmation: true, presence: true, uniqueness: {case_sensitive: false},format:{ with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i,
     message: "invalid email" }
+  validates :email_confirmation, presence: true
   validates :first_name, length: {minimum: 1}
   validates :last_name, presence: true
   validates :password, confirmation: true, format:{ with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/,message: "password must contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number"}
